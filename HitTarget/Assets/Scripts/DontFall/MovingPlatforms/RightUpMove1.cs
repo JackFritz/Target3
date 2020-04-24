9f90289e4c567f1864aca3d6beabb408fd7d44ac
JackFritz <jfritz@quinnipiac.edu>
2020-04-24 16:37:26 -0400

    Finished Level

---
+++ HitTarget/Assets/Scripts/DontFall/MovingPlatforms/RightUpMove1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightUpMove1 : MonoBehaviour
+{
+    float speed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed); // talking to the rigidbody of the platform and setting its velocity
+
+
+        if (transform.position.y > 1.53f)
+        {
+            transform.position = new Vector3(9.21f, -7.25f, 1000f);
+        }
+        //If the y position is greater than 1.53 it resets the y to -7.25
+    }
+}
---
+++ HitTarget/Assets/Scripts/DontFall/MovingPlatforms/RightUpMove2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightUpMove2 : MonoBehaviour
+{
+    float speed = 1f;
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed); // talking to the rigidbody of the platform and setting its velocity
+
+
+        if (transform.position.y > 3.75f)
+        {
+            transform.position = new Vector3(10.92f, -7.25f, 1000f);
+        }
+        //If the y position is greater than 3.75 it resets the y to -7.25
+    }
+}
